b1cbff9e426c7cb82596fdf2cdac71c55d711fe8
Nick Felt <nfelt1@sccs.swarthmore.edu>
2012-06-19 16:53:09 -0400

    make tagtest use getopt for options, support parameter tuning for tag detection

--- april/tagtest.cpp
+++ april/tagtest.cpp
@@ -1,40 +1,97 @@
-#include "TagDetector.h"
+#include <cstdio>
 #include <iostream>
+
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-int main(int argc, char** argv) {
+#include "TagDetector.h"
 
-  const std::string dstr = "--decimate";
+#define DEFAULT_TAG_FAMILY "Tag36h11"
 
-  if (argc < 3) {
-    std::cerr << "Usage: " << argv[0] << " TAGFAMILY [--decimate] IMAGE1 [IMAGE2 ...]\n";
-    std::cerr << "Known tag families:";
-    TagFamily::StringArray known = TagFamily::families();
-    for (size_t i=0; i<known.size(); ++i) { std::cerr << " " << known[i]; }
-    std::cerr << "\n";
-    return 1;
+typedef struct TagTestOptions {
+  TagTestOptions() :
+      show_timing(false),
+      show_verbose_debug(false),
+      generate_output_files(false),
+      params(),
+      family_str(DEFAULT_TAG_FAMILY) {
   }
-    
+  bool use_decimation;
+  bool show_timing;
+  bool show_verbose_debug;
+  bool generate_output_files;
+  TagDetectorParams params;
+  std::string family_str;
+} TagTestOptions;
 
+
+void print_usage(const char* tool_name, FILE* output=stderr) {
+  fprintf(output, "\
+Usage: %s [OPTIONS] IMAGE1 [IMAGE2 ...]\n\
+Run a tool to test tag detection. Options:\n\
+ -h              Show this help message.\n\
+ -d              Use decimation for segmentation stage.\n\
+ -t              Show timing information for tag detection.\n\
+ -v              Show verbose debug info from tag detection.\n\
+ -o              Generate debug visuals as output files vs. using X11.\n\
+ -s SEGSIGMA     Set the segmentation sigma value (default %.2f).\n\
+ -a THETATHRESH  Set the theta threshold for clustering (default %.1f).\n\
+ -m MAGTHRESH    Set the magnitude threshold for clustering (default %.1f).\n\
+ -f FAMILY       Look for the given tag family (default \"%s\")\n",
+          tool_name,
+          TagDetectorParams::kDefaultSegSigma,
+          TagDetectorParams::kDefaultThetaThresh,
+          TagDetectorParams::kDefaultMagThresh,
+          DEFAULT_TAG_FAMILY);
+
+  fprintf(output, "Known tag families:");
+  TagFamily::StringArray known = TagFamily::families();
+  for (size_t i = 0; i < known.size(); ++i) {
+    fprintf(output, " %s", known[i].c_str());
+  }
+  fprintf(output, "\n");
+}
+
+TagTestOptions parse_options(int argc, char** argv) {
+  TagTestOptions opts;
+  const char* option_str = "hdtvos:a:m:f:";
+  int c;
+  while ((c = getopt(argc, argv, option_str)) != -1) {
+    switch (c) {
+      case 'h': print_usage(argv[0], stdout); exit(0); break;
+      case 'd': opts.params.segDecimate = true; break;
+      case 't': opts.show_timing = true; break;
+      case 'v': opts.show_verbose_debug = true; break;
+      case 'o': opts.generate_output_files = true; break;
+      case 's': opts.params.segSigma = atof(optarg); break;
+      case 'a': opts.params.thetaThresh = atof(optarg); break;
+      case 'm': opts.params.magThresh = atof(optarg); break;
+      case 'f': opts.family_str = optarg; break;
+      default:
+        fprintf(stderr, "\n");
+        print_usage(argv[0], stderr);
+        exit(1);
+    }
+  }
+  return opts;
+}
+
+int main(int argc, char** argv) {
+  TagTestOptions opts = parse_options(argc, argv);
   const std::string win = "Tag test";
 
-  TagFamily family(argv[1]);
-  TagDetector detector(family);
-
-  detector.debug = false;
+  TagFamily family(opts.family_str);
+  TagDetector detector(family, opts.params);
+  detector.debug = opts.show_verbose_debug;
   detector.debugWindowName = win;
 
-  int sarg = 2;
-  if (argv[sarg] == dstr) {
+  if (opts.params.segDecimate) {
     std::cout << "will decimate for segmentation!\n";
-    detector.params.segDecimate = true;
-    ++sarg;
   }
 
   TagDetectionArray detections;
 
-  for (int i=sarg; i<argc; ++i) {
+  for (int i=optind; i<argc; ++i) {
 
     cv::Mat src = cv::imread(argv[i]);
     if (src.empty()) { continue; }
