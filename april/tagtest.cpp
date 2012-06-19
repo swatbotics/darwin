e55b0e043eb394f756f488889060d9d3ac7e24db
Nick Felt <nfelt1@sccs.swarthmore.edu>
2012-06-19 17:32:10 -0400

    clean up tagtest verbosity options and output

--- april/tagtest.cpp
+++ april/tagtest.cpp
@@ -10,15 +10,20 @@
 
 typedef struct TagTestOptions {
   TagTestOptions() :
+      show_debug_info(false),
       show_timing(false),
-      show_verbose_debug(false),
+      show_results(false),
+      be_verbose(false),
+      no_images(false),
       generate_output_files(false),
       params(),
       family_str(DEFAULT_TAG_FAMILY) {
   }
-  bool use_decimation;
+  bool show_debug_info;
   bool show_timing;
-  bool show_verbose_debug;
+  bool show_results;
+  bool be_verbose;
+  bool no_images;
   bool generate_output_files;
   TagDetectorParams params;
   std::string family_str;
@@ -30,10 +35,13 @@ void print_usage(const char* tool_name, FILE* output=stderr) {
 Usage: %s [OPTIONS] IMAGE1 [IMAGE2 ...]\n\
 Run a tool to test tag detection. Options:\n\
  -h              Show this help message.\n\
- -d              Use decimation for segmentation stage.\n\
+ -d              Show debug images and data during tag detection.\n\
  -t              Show timing information for tag detection.\n\
- -v              Show verbose debug info from tag detection.\n\
+ -r              Show textual results of tag detection.\n\
+ -v              Be verbose (includes -d -t -r).\n\
+ -x              Do not generate any non-debug visuals.\n\
  -o              Generate debug visuals as output files vs. using X11.\n\
+ -D              Use decimation for segmentation stage.\n\
  -s SEGSIGMA     Set the segmentation sigma value (default %.2f).\n\
  -a THETATHRESH  Set the theta threshold for clustering (default %.1f).\n\
  -m MAGTHRESH    Set the magnitude threshold for clustering (default %.1f).\n\
@@ -54,15 +62,19 @@ Run a tool to test tag detection. Options:\n\
 
 TagTestOptions parse_options(int argc, char** argv) {
   TagTestOptions opts;
-  const char* option_str = "hdtvos:a:m:f:";
+  const char* options_str = "hdtrvxoDs:a:m:f:";
   int c;
-  while ((c = getopt(argc, argv, option_str)) != -1) {
+  while ((c = getopt(argc, argv, options_str)) != -1) {
     switch (c) {
+      // Reminder: add new options to 'options_str' above and print_usage()!
       case 'h': print_usage(argv[0], stdout); exit(0); break;
-      case 'd': opts.params.segDecimate = true; break;
+      case 'd': opts.show_debug_info = true; break;
       case 't': opts.show_timing = true; break;
-      case 'v': opts.show_verbose_debug = true; break;
+      case 'r': opts.show_results = true; break;
+      case 'v': opts.be_verbose = true; break;
+      case 'x': opts.no_images = true; break;
       case 'o': opts.generate_output_files = true; break;
+      case 'D': opts.params.segDecimate = true; break;
       case 's': opts.params.segSigma = atof(optarg); break;
       case 'a': opts.params.thetaThresh = atof(optarg); break;
       case 'm': opts.params.magThresh = atof(optarg); break;
@@ -73,26 +85,25 @@ TagTestOptions parse_options(int argc, char** argv) {
         exit(1);
     }
   }
+  if (opts.be_verbose) {
+    opts.show_debug_info = opts.show_timing = opts.show_results = true;
+  }
   return opts;
 }
 
 int main(int argc, char** argv) {
-  TagTestOptions opts = parse_options(argc, argv);
   const std::string win = "Tag test";
-
+  TagTestOptions opts = parse_options(argc, argv);
   TagFamily family(opts.family_str);
   TagDetector detector(family, opts.params);
-  detector.debug = opts.show_verbose_debug;
+  detector.debug = opts.show_debug_info;
   detector.debugWindowName = win;
-
-  if (opts.params.segDecimate) {
-    std::cout << "will decimate for segmentation!\n";
+  if (opts.params.segDecimate && opts.be_verbose) {
+    std::cout << "Will decimate for segmentation!\n";
   }
 
   TagDetectionArray detections;
-
   for (int i=optind; i<argc; ++i) {
-
     cv::Mat src = cv::imread(argv[i]);
     if (src.empty()) { continue; }
 
@@ -101,41 +112,33 @@ int main(int argc, char** argv) {
       cv::resize(src, tmp, cv::Size(0,0), 0.5, 0.5);
       src = tmp;
     }
-
     cv::Point2d opticalCenter(0.5*src.rows, 0.5*src.cols);
 
-    if (!detector.debug) {
+    if (!detector.debug && !opts.no_images) {
       labelAndWaitForKey(win, "Original", src, ScaleNone);
     }
 
     clock_t start = clock();
-    
     detector.process(src, opticalCenter, detections);
-    
     clock_t end = clock();
-    
-    std::cout << "got " << detections.size() << " detections in " <<
-      double(end-start)/CLOCKS_PER_SEC << " seconds\n";
-    
-    for (size_t i=0; i<detections.size(); ++i) {
-      
-      const TagDetection& d = detections[i];
-      std::cout << "id = " << d.id << ", code=" << d.code << "\n";
-      std::cout << "rotation = " << d.rotation << "\n";
 
-      
-      
+    if (opts.show_results) {
+      if (opts.show_debug_info) std::cout << "\n";
+      std::cout << "Got " << detections.size() << " detections in "
+                << double(end-start)/CLOCKS_PER_SEC << " seconds.\n";
+      for (size_t i=0; i<detections.size(); ++i) {
+        const TagDetection& d = detections[i];
+        std::cout << " - Detection: id = " << d.id << ", "
+                  << "code = " << d.code << ", "
+                  << "rotation = " << d.rotation << "\n";
+      }
     }
-
-    cv::Mat img = family.superimposeDetections(src, detections);
-    labelAndWaitForKey(win, "Detected", img, ScaleNone);
-
+    if (!opts.no_images) {
+      cv::Mat img = family.superimposeDetections(src, detections);
+      labelAndWaitForKey(win, "Detected", img, ScaleNone);
+    }
   }
 
-  detector.reportTimers();
-
+  if (opts.show_timing) detector.reportTimers();
   return 0;
-
-  
-
 }
