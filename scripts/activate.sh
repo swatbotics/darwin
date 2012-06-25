d36f13e543a9972c9df3bf5d8fdc1a7f65df187a
Nick Felt <nfelt1@sccs.swarthmore.edu>
2012-06-25 12:07:45 -0400

    add virtualenv-style activate.sh script for setting path, commands, etc.

---
+++ scripts/activate.sh
@@ -0,0 +1,69 @@
+# Script to set up useful shell environment for working on darwin code.
+# Based largely on the /bin/activate script for virtualenv.
+
+# This file must be used with "source scripts/activate" *from bash*. It
+# can't be run directly or it won't be able to set environment variables.
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DARWIN_ROOT=$(dirname "$SCRIPT_DIR")
+export DARWIN_ROOT
+
+deactivate () {
+    # Restore the original path.
+    if [ -n "$_OLD_VIRTUAL_PATH" ] ; then
+        PATH="$_OLD_VIRTUAL_PATH"
+        export PATH
+        unset _OLD_VIRTUAL_PATH
+    fi
+
+    # This should detect bash and zsh, which have a hash command that must
+    # be called to get it to forget past commands.  Without forgetting
+    # past commands the $PATH changes we made may not be respected
+    if [ -n "$BASH" -o -n "$ZSH_VERSION" ] ; then
+        hash -r
+    fi
+
+    # Restore the original prompt.
+    if [ -n "$_OLD_VIRTUAL_PS1" ] ; then
+        PS1="$_OLD_VIRTUAL_PS1"
+        export PS1
+        unset _OLD_VIRTUAL_PS1
+    fi
+
+    # Get rid of 'deactivate' function itself.
+    if [ ! "$1" = "nondestructive" ] ; then
+    # Self destruct!
+        unset -f deactivate
+    fi
+}
+
+
+############################################
+##### Actual 'activate' part of script #####
+
+# Unset irrelevant variables.
+deactivate nondestructive
+
+# Add useful directories to the path.
+_OLD_VIRTUAL_PATH="$PATH"
+NEWPATH="$SCRIPT_DIR"
+BUILDBINPATH="$DARWIN_ROOT/build/bin"
+if [ -d "$BUILDBINPATH" ]; then
+   NEWPATH="$NEWPATH:$BUILDBINPATH"
+fi
+PATH="$NEWPATH:$PATH"
+export PATH
+
+# Change the prompt to show we're in a virtualenv.
+if [ -z "$VIRTUAL_ENV_DISABLE_PROMPT" ] ; then
+    _OLD_VIRTUAL_PS1="$PS1"
+    PS1="(darwin-code) $PS1"
+    export PS1
+fi
+
+# This should detect bash and zsh, which have a hash command that must
+# be called to get it to forget past commands.  Without forgetting
+# past commands the $PATH changes we made may not be respected
+if [ -n "$BASH" -o -n "$ZSH_VERSION" ] ; then
+    hash -r
+fi
