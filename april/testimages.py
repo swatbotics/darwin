fab3caa35303ddd8b9af34e5831b374ec25dca41
Nick Felt <nfelt1@sccs.swarthmore.edu>
2012-06-25 18:42:55 -0400

    update testimages.py to show new data for various detection improvements + for all 10 test images

--- april/testimages.py
+++ april/testimages.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+from __future__ import division
+
+import numpy as np
 from matplotlib import pyplot as plt
 import subprocess
 import sys
 
-TAGTEST_BIN = "build/tagtest"
+TAGTEST_BIN = "../build/bin/tagtest"
 
 TEST_IMAGES = [
     "images/darwincam/1-debug_0_0.png",
@@ -14,6 +17,9 @@ TEST_IMAGES = [
     "images/darwincam/5-debug_0_0.png",
     "images/darwincam/6-debug_0_0.png",
     "images/darwincam/7-debug_0_0.png",
+    "images/darwincam/8-debug_0_0.png",
+    "images/darwincam/9-debug_0_0.png",
+    "images/darwincam/10-debug_0_0.png",
 ]
 
 def get_image_counts(flags):
@@ -28,18 +34,38 @@ def parse_detection_counts(tagtest_output):
                  if line and line.split()[0] == 'Got')
 
 def main():
-    a_values = range(0, 100, 1)
-    ideal_counts = get_image_counts(['-rx'])
-    test_counts = [get_image_counts(['-rx', '-D', '-a %d' % a])
-                   for a in a_values]
-    all_data = zip(ideal_counts, zip(*test_counts))
-    for data_series in reversed(sorted(all_data)):
-        ideal = data_series[0]
-        actuals = data_series[1]
-        plt.plot(a_values, [ideal] * len(a_values), 'k:')
-        plt.plot(a_values, actuals, label='%d' % ideal)
-    plt.ylim(0, max(ideal_counts) + 5)
-    plt.legend()
+    tests = [
+        ('ideal', 'k', ['-Rx']),
+        ('+ decimation', 'r', ['-Rx', '-D']),
+        ('+ theta = 25', 'g', ['-Rx', '-D', '-a 25']),
+        ('+ corners', 'b', ['-Rx', '-D', '-a 25', '-c']),
+        ('+ subpix', 'y', ['-Rx', '-D', '-a 25', '-C']),
+    ]
+    all_data = [(label, color, get_image_counts(flags))
+                for label, color, flags in tests]
+    num_images = len(TEST_IMAGES)
+    num_series = len(tests)
+    inds = np.arange(num_images)
+    width = (1.0 - 0.2) / num_series
+
+    offset = 0
+    legend_handles = []
+    legend_labels = []
+    max_count = 0
+    for label, color, counts in all_data:
+        max_count = max(counts + (max_count,))
+        rectinfo = plt.bar(inds + offset, counts, width, color=color)
+        legend_handles += [rectinfo[0]]
+        legend_labels += [label]
+        offset += width
+
+    plt.legend(legend_handles, legend_labels)
+    plt.ylim(0, max_count + 25)
+    plt.xticks(inds + (width * num_series / 2),
+               ['#%d' % (n + 1) for n in range(len(TEST_IMAGES))])
+    plt.xlabel("Test images")
+    plt.ylabel("Number of detections")
+    plt.title("Improvements to Tag Detection")
     plt.show()
 
 if __name__ == '__main__':
