ba186d1b856fd632d3614491133f7d7a290d3baf
Nick Felt <nfelt1@sccs.swarthmore.edu>
2012-06-25 17:12:18 -0400

    add a 'sudow' wrapper script to get sudo playing nicely with path modifications done by activate.sh

---
+++ scripts/sudo_wrapper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if (( $# >= 1 )); then
+    # Pop off the first argument as the command to run.
+    COMMAND="$1"
+    shift 1
+    # Make sure we can find the command first.
+    if which "$COMMAND" >/dev/null; then
+	# Run sudo, using which to locate the command's real path.
+	sudo "$(which "$COMMAND")" "$@"
+    else
+	echo "Could not find command - is it a builtin?"
+	exit 1
+    fi
+else
+    # Nothing to run, so print usage.
+    echo "Usage: sudow <command> [args ...]"
+    echo "This command wraps sudo, using 'which' to find the full paths "
+    echo "to commands for the current user, without relying on root's PATH."
+    exit 1
+fi
