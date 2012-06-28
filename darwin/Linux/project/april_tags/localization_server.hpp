b4c7ec61323e6e80e8e83792831c7fe2c3d3defe
Nick Felt <nfelt1@sccs.swarthmore.edu>
2012-06-28 16:18:50 -0400

    wrote dummy draft of global localization server using asio

---
+++ darwin/Linux/project/april_tags/localization_server.hpp
@@ -0,0 +1,46 @@
+#ifndef LOCALIZATION_SERVER_HPP
+#define LOCALIZATION_SERVER_HPP
+
+#include <string>
+
+#include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "april/TagDetector.h"
+
+namespace boost {
+namespace asio {
+typedef boost::system::error_code error_code;
+typedef boost::thread thread;
+}  // namespace asio
+}  // namespace boost
+
+namespace asio = boost::asio;
+
+class LocalizationServer {
+ public:
+  LocalizationServer();
+  ~LocalizationServer() {}
+  void Run();
+
+ private:
+  void InitializeVideoDevice();
+  void RunLocalization();
+  void ReceiveRequest();
+  void HandleRequest(const asio::error_code& error,
+                     std::size_t /*bytes_transferred*/);
+  void HandleResponse(const asio::error_code& /*error*/,
+                      std::size_t /*bytes_transferred*/);
+
+  cv::VideoCapture vc_;
+  TagFamily tag_family_;
+  TagDetector detector_;
+  asio::io_service io_service_;
+  asio::ip::udp::socket socket_;
+  asio::ip::udp::endpoint remote_endpoint_;
+  boost::array<int, 1> recv_buffer_;
+  std::string dummy_response_;
+};
+
+#endif  // LOCALIZATION_SERVER_HPP
