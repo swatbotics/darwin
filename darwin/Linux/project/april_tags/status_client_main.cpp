ec15e49bb9eeca64233f6e95c5c8423f895e4dd8
Nick Felt <nfelt1@sccs.swarthmore.edu>
2012-07-15 14:54:29 -0400

    add printing_interval and quiet flags to status_client

--- darwin/Linux/project/april_tags/status_client_main.cpp
+++ darwin/Linux/project/april_tags/status_client_main.cpp
@@ -10,6 +10,10 @@ DEFINE_string(server_name, "192.168.1.7",
               "IP address or DNS name of the status server to query.");
 DEFINE_int32(server_port, 9000,
              "Port on the status server to connect to.");
+DEFINE_double(printing_interval, 0.01,
+              "Interval in seconds between printing out status data.");
+DEFINE_bool(quiet, false,
+            "Don't show the data received from the server.");
 
 int main(int argc, char* argv[]) {
   std::string usage;
@@ -19,8 +23,8 @@ int main(int argc, char* argv[]) {
   StatusClient client(FLAGS_server_name, FLAGS_server_port);
   client.Run();
   while (true) {
-    std::cout << client.GetData() << std::endl;
-    usleep(10*1000);
+    if (!FLAGS_quiet) std::cout << client.GetData() << std::endl;
+    usleep(1000 * 1000 * FLAGS_printing_interval);
   }
   client.Stop();
   return 0;
