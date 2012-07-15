51c76eac6209dfb4747de0e770ff3f705f776bc6
Nick Felt <nfelt1@sccs.swarthmore.edu>
2012-07-14 20:10:50 -0400

    add status_server target with dummy status server

---
+++ darwin/Linux/project/april_tags/status_server_main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+
+#include <gflags/gflags.h>
+
+#include "status_server.hpp"
+
+DEFINE_int32(server_port, 9000,
+             "Port on the status server to connect to.");
+DEFINE_double(update_interval, 0.1,
+              "Update interval of dummy data in seconds.");
+DEFINE_bool(quiet, false,
+            "Don't show the messages that the server is sending.");
+
+int main(int argc, char* argv[]) {
+  std::string usage;
+  usage += std::string("Usage: ") + argv[0] + std::string(" [OPTIONS]");
+  gflags::SetUsageMessage(usage);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  StatusServer server(FLAGS_server_port);
+  server.Run();
+  std::stringstream ss;
+  int message_count = 0;
+  while (true) {
+    ss.str(std::string());
+    ss << message_count++;
+    server.SetData(ss.str());
+    if (!FLAGS_quiet) std::cout << "Data = " << ss.str() << "\n";
+    usleep(1000 * 1000 * FLAGS_update_interval);
+  }
+  server.Stop();
+  return 0;
+}
